cca344a0ab29ef7c4940238de70c1d7ef98adf7d
Billy Mohajeri <billy.mohajeri@gmail.com>
2025-01-26 12:24:36 +0200

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,8 @@
-content: ['./src/**/*.{js,jsx,ts,tsx}'],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
